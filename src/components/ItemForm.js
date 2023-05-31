6c378f6b1c07814fb43b4588e21edd41b9f9c6aa
donvine mugendi <donvinemugendi@gmail.com>
2023-06-01 00:12:44 +0300

    updated functions

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ handleFormSubmit }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (category === "" && name === "") {
+      return;
+    }
+    const NewItem = { name:name, category: category, isInCart: false };
+    console.log(NewItem);
+
+    handleFormSubmit(NewItem);
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
@@ -21,8 +32,7 @@ function ItemForm() {
         <select
           name="category"
           value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
+          onChange={(e) => setCategory(e.target.value)}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
+
+const baseUrl = "http://localhost:4000/items";
+
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  //useEffect function
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(baseUrl);
+      const data = await response.json();
+      setItems(data);
+    })();
+    return () => {
+      //clean up function here
+    };
+  }, []);
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -17,16 +32,48 @@ function ShoppingList() {
     return item.category === selectedCategory;
   });
 
+  function handleFormSubmit(NewItem) {
+    fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(NewItem),
+    })
+      .then((response) => response.json())
+      .then((data) => setItems((prevItems)=>[...prevItems,data]));
+  }
+  function handleAddToCart(item){
+    fetch(`${baseUrl}/${item.id}`,{
+      method: 'PATCH',
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      })
+    }).then(res=>res.json())
+    .then((data)=>console.log(data));
+  }
+
+  function handleItemDelete(item){
+    fetch(`${baseUrl}/${item.id}`,{
+      method: 'DELETE',
+    }).then(res=>res.json())
+    .then(setItems(items.filter((items)=>items !== item)));
+
+  }
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm handleFormSubmit={handleFormSubmit} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onAddToCart={handleAddToCart} onDelete={handleItemDelete}/>
         ))}
       </ul>
     </div>
